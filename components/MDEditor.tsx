731b7c22d87dae7abbbc53df703d12f5279b52df
Mahmudul Alam <mahmudula2000@gmail.com>
2024-06-14 17:49:10 +0600

    improved  MDEditor

--- components/MDEditor.tsx
+++ components/MDEditor.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Options } from "easymde";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
@@ -15,6 +17,67 @@ interface Props {
 }
 
 const MDEditor = ({ name, error, onChange, placeholder, value }: Props) => {
+  const customToolbar = useMemo(() => {
+    return {
+      toolbar: [
+        "bold",
+        "italic",
+        "code",
+        {
+          name: "headers",
+          className: "fa fa-header",
+          title: "Headers",
+          children: [
+            "heading-1",
+            "heading-2",
+            "heading-3",
+            "heading-bigger",
+            "heading-smaller",
+          ],
+        },
+        "link",
+        "image",
+        "redo",
+        "undo",
+        {
+          name: "super-script",
+          action: function (editor: any) {
+            var cm = editor.codemirror;
+            cm.replaceSelection("<sup>" + cm.getSelection() + "</sup>");
+            cm.focus();
+          },
+          className: "fa fa-superscript",
+          title: "Superscript",
+        },
+        {
+          name: "sub-script",
+          action: function (editor: any) {
+            var cm = editor.codemirror;
+            cm.replaceSelection("<sub>" + cm.getSelection() + "</sub>");
+            cm.focus();
+          },
+          className: "fa fa-subscript",
+          title: "Subscript",
+        },
+        {
+          name: "others",
+          className: "fa fa-ellipsis-h",
+          title: "Other Buttons",
+          children: [
+            "table",
+            "unordered-list",
+            "ordered-list",
+            "horizontal-rule",
+            "quote",
+            "guide",
+            "strikethrough",
+          ],
+        },
+        "preview",
+        "side-by-side",
+      ],
+    } as Options;
+  }, []);
   return (
     <div className="w-full h-auto">
       <SimpleMDE
@@ -23,6 +86,7 @@ const MDEditor = ({ name, error, onChange, placeholder, value }: Props) => {
         placeholder={placeholder}
         className="h-auto"
         onChange={onChange}
+        options={customToolbar}
       />
       {error && <div className="alert-error">{error}</div>}
     </div>
