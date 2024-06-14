7893148db5bfdf04caa7405af4a314128b96da63
Mahmudul Alam <mahmudula2000@gmail.com>
2024-06-14 19:25:04 +0600

    updated denied page

--- app/denied/page.tsx
+++ app/denied/page.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Denied = () => {
   return (
-    <div>
-      <h1 className="text-error">Permission Denied</h1>
-      <p>
-        You don&apos;t have enough permission to perform requested action or
-        view requested content. If you think this is a mistake please contact
-        admin.
-      </p>
+    <div className="both-center">
+      <div className="block max-w-2xl m-5 h-32 text-center">
+        <h1 className="text-error">Permission Denied</h1>
+        <p className="mt-5">
+          You don&apos;t have enough permission to perform requested action or
+          view requested content. If you think this is a mistake please contact
+          admin.
+        </p>
+      </div>
     </div>
   );
 };
